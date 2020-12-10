cbd910addfedc3fff1512fb678566af617d2ff9c
Takumi Uchida <takumi.u0521@gmail.com>
2020-12-10 11:25:53 +0900

    vue課題①

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="HelloWorld"/>
   </div>
 </template>
 
@@ -21,7 +20,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: center;xddff   
   color: #2c3e50;
   margin-top: 60px;
 }
